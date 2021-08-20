91142eb792d87a52698b598a4dbdd6cd41a91854
Sagar Gupta <sagargupta1827@gmail.com>
2021-08-20 09:38:16 +0545

    ADD: Google Maps API

--- web/public/carpage.js
+++ web/public/carpage.js
@@ -165,3 +165,34 @@ function handleSeat(boolValue) {
       console.error(`Error: ${error}`);
   });
 }
+
+let map;
+
+function initMap() {
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 7,
+      center: { lat: 41.85, lng: -87.65 },
+    });
+    directionsRenderer.setMap(map);
+  
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+  }
+  
+  function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+    directionsService
+      .route({
+        origin: {
+          query: "Melbourne",
+        },
+        destination: {
+          query: "Sydney",
+        },
+        travelMode: google.maps.TravelMode.DRIVING,
+      })
+      .then((response) => {
+        directionsRenderer.setDirections(response);
+      })
+      .catch((e) => window.alert("Directions request failed due to " + status));
+  }
