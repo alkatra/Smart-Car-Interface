a78bfe94efb895d05908c5bce5fbb900360c5898
Sagar Gupta <sagargupta1827@gmail.com>
2021-08-29 10:20:07 +0545

    ADD: Error Handling for all API Endpoints

--- api/controller/controller.js
+++ api/controller/controller.js
@@ -5,11 +5,15 @@ exports.addRoom = (req,res) => {{
     roomName = req.body.roomname;
     climSetting = 24;
     lights = [];
-    userDB.findOneAndUpdate({"username": username}, {$push: {"rooms": {
-        "roomName": roomName,
-        "climSetting": climSetting,
-        "lights": lights
-    }}}).then(res.status(200).send({message: `done`}));
+    try {
+        userDB.findOneAndUpdate({"username": username}, {$push: {"rooms": {
+            "roomName": roomName,
+            "climSetting": climSetting,
+            "lights": lights
+        }}}).then(res.status(200).send({message: `done`}));
+    } catch(e) {
+        res.status(400).send({message: e})
+    }
 }}
 
 exports.addCar = (req,res) => {{
@@ -18,38 +22,54 @@ exports.addCar = (req,res) => {{
     climSetting = req.body.climSetting;
     seatSetting = req.body.seatSetting;
     lightColor = req.body.lightColor;
-    workAddress = req.body.workAddress;
-    userDB.findOneAndUpdate({"username": username}, {$push: {"cars": {
-        "carName": carName,
-        "climSetting": climSetting,
-        "seatSetting": seatSetting,
-        "lightColor": lightColor,
-        "workAddress": workAddress
-    }}}).then(res.status(200).send({message: `done`}));
+    workAddress = req.body.workAddress;\
+    try {
+        userDB.findOneAndUpdate({"username": username}, {$push: {"cars": {
+            "carName": carName,
+            "climSetting": climSetting,
+            "seatSetting": seatSetting,
+            "lightColor": lightColor,
+            "workAddress": workAddress
+        }}}).then(res.status(200).send({message: `done`}));
+    } catch(e) {
+        res.status(400).send({message: e})
+    }
 }}
 
 exports.updateTemp = (req,res) => {{
     carName = req.body.carName_;
     username =  req.body.username_;
     newtemp = req.body.newtemp;
-    userDB.findOneAndUpdate({"username": username, "cars.carName": carName}, {$set: {"cars.$.climSetting": newtemp}}).then(
-        res.status(200).send({message: `done`}));
+    try {
+        userDB.findOneAndUpdate({"username": username, "cars.carName": carName}, {$set: {"cars.$.climSetting": newtemp}}).then(
+            res.status(200).send({message: `done`}));
+    } catch(e) {
+        res.status(400).send({message: e})
+    }
 }}
 
 exports.updateSeat = (req,res) => {{
     carName = req.body.carName_;
     username =  req.body.username_;
     newsetting = req.body.newsetting;
-    userDB.findOneAndUpdate({"username": username, "cars.carName": carName}, {$set: {"cars.$.seatSetting": newsetting}}).then(
-        res.status(200).send({message: `done`}));
+    try {
+        userDB.findOneAndUpdate({"username": username, "cars.carName": carName}, {$set: {"cars.$.seatSetting": newsetting}}).then(
+            res.status(200).send({message: `done`}));
+    } catch(e) {
+        res.status(400).send({message: e})
+    }
 }}
 
 exports.changeCarColor = (req,res) => {{
     carName = req.body.carName_;
     username =  req.body.username_;
     newcolor = req.body.newcolor;
-    userDB.findOneAndUpdate({"username": username, "cars.carName": carName}, {$set: {"cars.$.lightColor": newcolor}}).then(
-        res.status(200).send({message: `done`}));
+    try {
+        userDB.findOneAndUpdate({"username": username, "cars.carName": carName}, {$set: {"cars.$.lightColor": newcolor}}).then(
+            res.status(200).send({message: `done`}));
+    } catch(e) {
+        res.status(400).send({message: e})
+    }
 }}
 
 exports.changeRoomColor = (req,res) => {{
@@ -57,6 +77,7 @@ exports.changeRoomColor = (req,res) => {{
     lightName = req.body.lightName;
     roomname = req.body.roomname_;
     newcolor = req.body.newcolor;
+    try {
     userDB.findOneAndUpdate(
         {"username": username, "rooms.roomName": roomname}, 
         {
@@ -75,23 +96,34 @@ exports.changeRoomColor = (req,res) => {{
         }
     ).then(
         res.status(200).send({message: `done`}));
+    } catch(e) {
+        res.status(400).send({message: e})
+    }
 }}
 
 exports.changeRoomTemp = (req,res) => {{
     roomname = req.body.roomname_;
     username =  req.body.username_;
     newtemp = req.body.newtemp;
-    userDB.findOneAndUpdate({"username": username, "rooms.roomName": roomname}, {$set: {"rooms.$.climSetting": newtemp}}).then(
-        res.status(200).send({message: `done`}));
+    try {
+        userDB.findOneAndUpdate({"username": username, "rooms.roomName": roomname}, {$set: {"rooms.$.climSetting": newtemp}}).then(
+            res.status(200).send({message: `done`}));
+    } catch(e) {
+        res.status(400).send({message: e})
+    }
 }}
 
 exports.addLight = (req,res) => {{
     username = req.body.username_;
     lightName = req.body.lightName;
     roomname = req.body.roomname_;
-    userDB.findOneAndUpdate({"username": username, "rooms.roomName": roomname}, {$push: {"rooms.$.lights": {
-        "Name": lightName,
-        "Color": "lightblue"
-    }}}).then(
-        res.status(200).send({message: `done`}));
+    try {
+        userDB.findOneAndUpdate({"username": username, "rooms.roomName": roomname}, {$push: {"rooms.$.lights": {
+            "Name": lightName,
+            "Color": "lightblue"
+        }}}).then(
+            res.status(200).send({message: `done`}));
+    } catch(e) {
+        res.status(400).send({message: e})
+    }
 }}
